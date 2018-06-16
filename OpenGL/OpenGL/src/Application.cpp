297da11445f8280f0eee21afe9ebb09a1a18e64d
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-06-16 19:26:33 +0100

    added modern opengl support

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -18,13 +18,14 @@ int main(void)
 		return -1;
 	}
 
-	if(glewInit()!=GLEW_OK)
-	{
-		printf("Error Initializing GLEW\n");
-	}
-
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Error Initializing GLEW\n");
+		return -1;
+	}
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
