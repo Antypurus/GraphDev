72c74bc84cee6b8ca255bcf60a629db1365a724e
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-06-16 23:53:47 +0100

    moved basic shader to single file and added function that separates the file into various shader sources and parses it

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -1,6 +1,52 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream file(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0,FRAGMENT =1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while(getline(file,line))
+	{
+		if(line.find("#shader") != std::string::npos)
+		{
+			if(line.find("vertex")!=std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if(line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			if (type != ShaderType::NONE)
+			{
+				ss[(unsigned int)type] << line << '\n';
+			}
+		}
+	}
+
+	return { ss[0].str(),ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source,const std::string& name)
 {
@@ -88,28 +134,11 @@ int main(void)
 
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); //sets the data that is being used
 
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+	std::cout << source.VertexSource << std::endl;
+	std::cout << source.FragmentSource << std::endl;
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
-
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
