aa811c0fa40a61ec5ed39aa3858c236d942994ef
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-06-16 23:04:17 +0100

    added function for compiling shader and to create shader program

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -2,6 +2,49 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source,const std::string& name)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	/*Error Handling*/
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if(result==GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+		char* message = (char*)alloca(length*sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		
+		printf("Failed to compile %s shader. Error:%s", name.c_str(), message);
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader,const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader,"Vertex");
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader,"Fragment");
+	
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -55,7 +98,6 @@ int main(void)
 		/* Draw A Triangle By issuing draw call to buffer */
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
-
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
