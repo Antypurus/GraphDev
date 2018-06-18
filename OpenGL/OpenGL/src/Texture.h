8c64f4a497eb8c001f00574f57aaa8c19b28315f
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-06-18 02:09:20 +0100

    added texture class to handle textures

---
+++ OpenGL/OpenGL/src/Texture.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Renderer.h"
+
+class Texture
+{
+private:
+	unsigned int	m_RendererID;
+	std::string		m_Filepath;
+	unsigned char*	m_LocalBuffer;
+	int				m_Width;
+	int				m_Height;
+	int				m_BPP;
+public:
+	Texture(const std::string& filepath);
+	~Texture();
+
+	void Bind()const;
+	void Unbind()const;
+
+	inline int GetWidth() const { return m_Width; }
+	inline int getHeiht() const { return m_Height; }
+};
