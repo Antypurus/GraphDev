a7b0c549349685105ad5b765c5cc88c9348bd9f6
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-06-19 16:29:46 +0100

    added makeshift model view projection matrix

--- OpenGL/OpenGL/src/Application.cpp
+++ OpenGL/OpenGL/src/Application.cpp
@@ -23,7 +23,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Graphics Development Window", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Graphics Development Window", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -44,10 +44,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << "\n";
 	{
 		float positions[] = {
-			-0.5f, -0.5f, 0.0f, 0.0f,   //0
-			 0.5f, -0.5f, 1.0f, 0.0f,   //1
-			 0.5f,  0.5f, 1.0f, 1.0f,   //2
-			-0.5f,  0.5f, 0.0f, 1.0f	//3
+			100.0f, 100.0f, 0.0f, 0.0f,   //0
+			200.0f, 100.0f, 1.0f, 0.0f,   //1
+			200.0f, 200.0f, 1.0f, 1.0f,   //2
+			100.0f, 200.0f, 0.0f, 1.0f	//3
 		};
 
 		unsigned int indices[] = {
@@ -68,11 +68,15 @@ int main(void)
 
 		IndexBuffer ib(indices, 6);
 
-		glm::mat4 proj = glm::ortho(-2.0f,2.0f,-1.5f,1.5f,-1.0f,1.0f);
+		glm::mat4 proj = glm::ortho(0.0f,960.0f,0.0f,540.0f,-1.0f,1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+		glm::mat4 mvp = proj * view * model;;
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/adromeda.png");
 		texture.Bind();
