99b0983ed47be2a5ce271b057435e0f26adbb316
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-06-22 20:49:25 +0100

    added test scene for per pixel ligthing

---
+++ OpenGL/OpenGL/src/tests/TestPerPixelBasicLigthing.cpp
@@ -0,0 +1,16 @@
+﻿#include "TestPerPixelBasicLigthing.h"
+
+void Test::TestPerPixelBasicLigthing::OnUpdate(float deltaTime)
+{
+
+}
+
+void Test::TestPerPixelBasicLigthing::OnRender()
+{
+
+}
+
+void Test::TestPerPixelBasicLigthing::OnImGuiRender()
+{
+
+}
---
+++ OpenGL/OpenGL/src/tests/TestPerPixelBasicLigthing.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Test.h"
+
+namespace Test
+{
+	class TestPerPixelBasicLigthing : public Test
+	{
+	public:
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+	};
+}
